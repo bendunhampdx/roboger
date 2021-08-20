2abd427fbe1a9d115c540e790270c084be58bd1e
bendunhampdx <bendunhampdx@gmail.com>
2021-08-20 10:26:33 -0700

    get first test to pass showing a range of numbers in the console

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,18 @@
-
+function roboger(input) {
+  // let number = $("#userInput").val();
+  let range = [];
+  for (let i = 1; i <= input; i++) {
+    range.push(i);
+  };
+  console.log("are we here??");
+  return range;
+};
 
 
 $(document).ready(function() {
   $("#number").submit(function(event) {
     event.preventDefault();
-    $("#output").text(range)
-  })
-  function roboger() {
-    let range = [];
-    for (let i = 0; i <= $("#userInput").length; i++) {
-      range.push(i);
-    }
-  }
-  roboger();
-})
+    
+    $("#output").append(range[i]);
+  });
+});
